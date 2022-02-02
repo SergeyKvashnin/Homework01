ee5d9ae9a1a5e63a3ac287cfa614cf26145280d9
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-02-02 21:48:26 +0300

    Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Найти максимальное из трёх чисел.
+Console.Clear();
+
+int a = 15;
+int b = 87;
+int c = 95;
+
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
