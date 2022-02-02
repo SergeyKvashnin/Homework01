fd6ee7eb5fb064d72dca02a7dd652f53c596eebc
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-02-02 21:44:06 +0300

    Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Даны два числа. Показать большее и меньшее число
+Console.Clear();
+
+int  numberA=99,
+    numberB=100;
+if (numberA>numberB) 
+{
+    Console.WriteLine("Максимальное число numberA, Минимальное число numberB");
+}
+else
+{
+    Console.WriteLine("Максимальное число numberB, Минимальное число numberА");
+}
